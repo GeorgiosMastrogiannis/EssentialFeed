cc58a86682c4a1d21cc0e77cbe971d9c97d5d5f2
Georgios Mastrogiannis <georgios.mastrogiannis@merkle.com>
2023-06-03 18:33:28 +0200

    Give a url instead of giving it to RemoteFeedLoader

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -2,11 +2,13 @@ import XCTest
 
 class RemoteFeedLoader {
     let client: HTTPClient
-    init (client: HTTPClient) {
+    let url: URL
+    init (url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
     func load() {
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -23,17 +25,19 @@ class HTTPClientSpy: HTTPClient {
 
 class RemoteFeedLoadertests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: url,client: client)
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
