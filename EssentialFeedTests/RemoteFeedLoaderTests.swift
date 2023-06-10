739800cd205be63f3370f2ed1f3f9f22c7be80e9
Georgios Mastrogiannis <georgios.mastrogiannis@merkle.com>
2023-06-10 18:01:07 +0200

    Guarantee we can load more than once

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -4,7 +4,6 @@ import EssentialFeed
 class RemoteFeedLoadertests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
-        
         XCTAssertNil(client.requestedURL)
     }
     
@@ -12,12 +11,18 @@ class RemoteFeedLoadertests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         sut.load()
-        
         XCTAssertEqual(client.requestedURL, url)
     }
     
-    // MARK: - Helpers
+    func test_loadTwice_requestsDataFromURLtwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        sut.load()
+        sut.load()
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
     
+    // MARK: - Helpers
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
@@ -26,9 +31,11 @@ class RemoteFeedLoadertests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }
